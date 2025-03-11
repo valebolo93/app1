2929c679d5bf77be72dc0fe1a9b6c1b755771b3f
valebolo93 <valebolo93@gmail.com>
2025-03-11 13:41:28 +0100

    utils

---
+++ src/utils/utils.py
@@ -0,0 +1,11 @@
+def utility_function_1():
+    print("utility function_1")
+    return 1
+
+def utility_function_2(param1,param2):
+    print("utility function_2")
+    return param1 + param2
+
+
+#if __name__ == "__main__":
+utility_function_2(1,2)
